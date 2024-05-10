a436d44f9a17fcafde73d9916fcdd45beb1f2677
Hard Joshi <hardjoshi.dev@gmail.com>
2024-05-10 03:21:43 +0100

    Create responsive.js

---
+++ javascript/responsive.js
@@ -0,0 +1,26 @@
+/* ###################################
+    ----- CODE FOR RESPONSIVE PAGES -----
+################################### */
+// Function to check if the screen size is that of a mobile phone
+function isMobileScreen() {
+  return window.matchMedia("(max-width: 768px)").matches;
+}
+
+// Function to redirect to constructionPage.html
+function redirectToConstructionPage() {
+  window.location.href = "constructionPage.html";
+}
+
+// Check screen size on page load
+window.addEventListener("load", function () {
+  if (isMobileScreen()) {
+    redirectToConstructionPage();
+  }
+});
+
+// Check screen size on window resize
+window.addEventListener("resize", function () {
+  if (isMobileScreen()) {
+    redirectToConstructionPage();
+  }
+});
